128eee7e9bd12ab46fd45073f133b9e30cdec1a4
Maria Mireles <mireles335@gmail.com>
2021-06-20 21:19:47 -0500

    api/ adding event listener

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,35 +1,37 @@
-var cityNameEl = document.querySelector("#city-name");
-//variables to store a reference to the <form> el and id of user form
-var userFormEl = document.querySelector("#user-form");
-var nameInputEl = document.querySelector("#username");
-fetch(
-  `https://api.openweathermap.org/data/2.5/weather?q=austin&appid=65f5c26cfffe1860cc7a2aca1a96630d`
-)
-  .then((response) => {
-    return response.json();
-  })
-  .then((data) => {
-    console.log(data);
+
+// //variables to store a reference to the <form> el and id of user form
+var cityFormEl = document.querySelector("#city-form");
+var nameInputEl = document.querySelector("#city-name");
+var apiKey = "65f5c26cfffe1860cc7a2aca1a96630d"
+
+var apiUrl = "https://api.openweathermap.org/data/2.5/weather?id=524901&lang=fr&appid=" + apiKey;
+
+//formatting the response in the fetch logic 
+fetch(apiUrl)
+
+  .then(function (response) {
+    response.json().then(function(data) {
+        console.log(data);
+      })
+  
   });
 
+  
 //take lat and lon data point from first fetch request to make second api call
 
-var getCity = function () {
-    
-  // grab city name from url query string
-  var queryString = document.location.search;
-  var cityName = queryString.split("=")[1];
 
-  if (cityName) {
-    //display city name on the page
-    cityNameEl.textContent = cityName;
-  }
-};
 
-// /// form submission event function 
-// var formSubmitHandler =  function(event) {
-//     //prevent page from refreshing 
-//     event.preventDefault();
-//     //value of input elment 
-//     console.log(event);
-// }
+/// form submission event function 
+var formSubmitHandler =  function(event) {
+    //prevent page from refreshing 
+    event.preventDefault();
+      // get value from input element
+
+
+    console.log(event);
+}
+
+// add event listeners to forms
+cityFormEl.addEventListener("submit", formSubmitHandler);
+//languageButtonsEl.addEventListener("click", buttonClickHandler);
+//
